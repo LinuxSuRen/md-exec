cc9fca75eb0303886bd213e0b1cef38212eb1c9e
rick <LinuxSuRen@users.noreply.github.com>
2023-02-15 09:06:31 +0800

    chore: add codacy test coverage reporting

--- cli/root_test.go
+++ cli/root_test.go
@@ -41,3 +41,29 @@ func TestIsInputRequest(t *testing.T) {
 		})
 	}
 }
+
+func TestNewRootCommand(t *testing.T) {
+	tests := []struct {
+		name   string
+		args   []string
+		hasErr bool
+	}{{
+		name:   "no argument",
+		args:   nil,
+		hasErr: true,
+	}, {
+		name:   "more than one argument",
+		args:   []string{"a", "b"},
+		hasErr: true,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := NewRootCommand()
+			assert.True(t, cmd.HasExample())
+
+			cmd.SetArgs(tt.args)
+			err := cmd.Execute()
+			assert.Equal(t, tt.hasErr, err != nil)
+		})
+	}
+}
